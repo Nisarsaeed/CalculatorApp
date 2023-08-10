c28a08d389caa4d5df1b068a09ad458886985323
Nisar Saeed <dash75549@gmail.com>
2023-08-10 23:10:57 +0500

    feat: JS Functionality added

---
+++ script.js
@@ -0,0 +1,54 @@
+let currentValue = '';
+let currentOperator = '';
+let result = 0;
+
+function inputNum(value) {
+  currentValue += value;
+  document.getElementById('output').textContent = currentValue;
+}
+
+function inputOperator(oper) {
+  if (currentValue !== '') {
+    calculate();
+    currentOperator = oper;
+  }
+}
+
+function calculate() {
+  if (currentValue === '') return;
+  switch (currentOperator) {
+    case '+':
+      result += parseFloat(currentValue);
+      break;
+    case '-':
+      result -= parseFloat(currentValue);
+      break;
+    case '*':
+      result *= parseFloat(currentValue);
+      break;
+    case '/':
+      result /= parseFloat(currentValue);
+      break;
+      case '%': // Percentage calculation
+      result = (result * parseFloat(currentValue)) / 100;
+      break;
+    default:
+      result = parseFloat(currentValue);
+  }
+  currentValue = '';
+  document.getElementById('output').textContent = result;
+}
+
+function reset() {
+  currentValue = '';
+  currentOperator = '';
+  result = 0;
+  document.getElementById('output').textContent = result;
+}
+
+function toggleNegate() {
+    if (currentValue !== '') {
+        currentValue = (-parseFloat(currentValue)).toString();
+        document.getElementById('output').textContent = currentValue;
+    }
+}
